802db1605e7d4f6aa71d7c92176cd07a5c42bc52
devwz <wellingtonantonio_zaneze@live.com>
2018-10-08 21:33:11 -0300

    add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { ROUTES } from './app.routes';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
@@ -9,6 +11,7 @@ import { FooterComponent } from './footer/footer.component';
 import { LancheComponent } from './lanche/lanche.component';
 import { BebidasComponent } from './bebidas/bebidas.component';
 import { IngredientesComponent } from './ingredientes/ingredientes.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -22,7 +25,8 @@ import { IngredientesComponent } from './ingredientes/ingredientes.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
